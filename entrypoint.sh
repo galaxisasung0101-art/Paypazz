dd1a30e56fe46493220082a731359dfe542ceb0c
galaxisasung0101-art <galaxisasung0101@gmail.com>
2026-07-02 15:50:52 +0700

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -z "$PORT" ]; then
-    PORT=8080
-fi
-
-sed -i "s/Listen 80/Listen $PORT/" /etc/apache2/ports.conf
-sed -i "s/:80/:$PORT/" /etc/apache2/sites-available/000-default.conf
-
-apache2-foreground
