87709a0184da8625bc6a07cae43ca75ac987a5b2
galaxisasung0101-art <galaxisasung0101@gmail.com>
2026-07-02 18:14:10 +0700

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -n "$PORT" ]; then
-    sed -i "s/80/${PORT}/g" /etc/apache2/sites-available/000-default.conf /etc/apache2/ports.conf
-fi
-
-# Cek config biar gak crash lagi
-apache2ctl -t
-
-exec "$@"
